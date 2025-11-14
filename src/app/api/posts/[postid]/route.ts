03f35f2e20b4f752c6da9684e6f501002946c0b3
Subham srijit Lenka <subhamsrijit19@gmail.com>
2025-11-14 21:12:23 +0530

    delete post

---
+++ src/app/api/posts/[postid]/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import dbConnect from "@/lib/dbConnect";
+import PostModel from "@/models/Posts";
+import { verifyToken } from "@/lib/verifyToken";
+import cloudinary from "@/lib/cloudinary";
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { postId: string } }
+) {
+  try {
+    await dbConnect();
+
+    const { valid, decodedToken } = await verifyToken(req as any);
+    if (!valid) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const post = await PostModel.findById(params.postId);
+    if (!post) {
+      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    if (post.user.toString() !== decodedToken?._id) {
+      return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+    }
+
+    const deleteCloudImage = (url: string) => {
+      const publicId = url.split("/").pop()?.split(".")[0];
+      return cloudinary.uploader.destroy("nexorbitx_posts/" + publicId);
+    };
+
+    await Promise.all(post.images.map(deleteCloudImage));
+
+    await PostModel.findByIdAndDelete(params.postId);
+
+    return NextResponse.json(
+      { message: "Post deleted successfully" },
+      { status: 200 }
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Failed to delete post" },
+      { status: 500 }
+    );
+  }
+}
