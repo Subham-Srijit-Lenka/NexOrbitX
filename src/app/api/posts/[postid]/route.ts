6e8dcdc46d8824055cebd1013f21c064ebf73e3e
Subham srijit Lenka <subhamsrijit19@gmail.com>
2025-11-16 20:28:36 +0530

    update

--- src/app/api/posts/[postid]/route.ts
+++ src/app/api/posts/[postid]/route.ts
@@ -3,6 +3,7 @@ import dbConnect from "@/lib/dbConnect";
 import PostModel from "@/models/Posts";
 import { verifyToken } from "@/lib/verifyToken";
 import cloudinary from "@/lib/cloudinary";
+import { tryLoadManifestWithRetries } from "next/dist/server/load-components";
 
 export async function DELETE(
   req: NextRequest,
@@ -41,6 +42,39 @@ export async function DELETE(
   } catch (err) {
     return NextResponse.json(
       { error: "Failed to delete post" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { postId: string } }
+) {
+  try {
+    await dbConnect();
+
+    const { valid, decodedToken } = await verifyToken(req as any);
+
+    if (!valid) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const post = await.PostModel.findById(params.postId);
+    if(!post){
+        return NextResponse.json({error: "Post not found"}, {status: 401});
+        }
+
+     if (post.user.toString() !== decodedToken?._id) {
+            return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+          }
+          return NextResponse.json(
+            { message: "Post updated successfully" },
+            { status: 200 }
+    }
+    catch (error) {
+    return NextResponse.json(
+      { error: "failed to update post" },
       { status: 500 }
     );
   }
